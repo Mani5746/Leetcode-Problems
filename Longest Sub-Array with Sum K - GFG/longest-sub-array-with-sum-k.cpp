a1c0aa0ec046c6ade940e697382c1c40ade6efa3
Mani5746 <84321476+Mani5746@users.noreply.github.com>
2022-12-06 16:00:00 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int lenOfLongSubarr(int a[],  int n, int k) 
+    { 
+        // Complete the function
+        unordered_map<int,int> mp;
+        long long sum=0;
+        int maxlen=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            sum+=a[i];
+            
+            if(sum==k)
+            maxlen=i+1;
+            
+            if(mp.find(sum)==mp.end())
+            {
+                mp[sum]=i;
+            }
+            
+            if(mp.find(sum-k)!=mp.end())
+            {
+                if(maxlen<(i-mp[sum-k]))
+                maxlen=i-mp[sum-k];
+            }
+            
+        }
+        
+        return maxlen;
+    } 
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;cin>>t;
+	while(t--)
+	{
+	    int n, k;
+	    cin>> n >> k;
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution ob;
+	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
